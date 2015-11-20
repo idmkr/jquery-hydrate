a5d11cfc267e17b5bc3fd97f35244a5e770c41dd
zzgael <zzgael@gmail.com>
2015-11-20 11:08:29 +0100

    Update jquery.hydrate.js

--- jquery.hydrate.js
+++ jquery.hydrate.js
@@ -1,32 +1,48 @@
-
 (function ($) {
-  $.fn.hydrate = function (options,callback) {
-  	$(this).bind({
-  		keyup : function (e) { e.keyCode === 13 && $(this).blur() },
-  		change :function ()  { 
-  			var $t = $(this), o = { data : {} }, hasData = false;
-  			
-  			o.data[$t.attr("name") || "value"] = 
-  				$t.is(':checkbox,:radio') ? ( $t.attr("value") || $t[0].checked ) : $t.val();
-  
-  			typeof options == "object" && $.each(options, function (k,v) {
-  				if( k.match(/post|get/i) )
-  					o.method = k, o.url = v;
-  				else if( k.match( /dataattributes/i ) )
-  					$.map(v, function (d) { o.data[d] = $t.data(d) }) && hasData = true;
-  				else if( k.match( /success|error/i ) && typeof v == "function" )
-  					o[k]=$.proxy(v,$t[0]);
-  				else 
-  					o[k] = v;
-  			}) || (
-  				o.url = options,
-  				typeof callback == "function" && o.success = $.proxy(callback,$t[0])
-  			);
-  			
-  			hasData || $.extend(o.data,$t.data());
-  
-  			$.ajax(o);
-  		}
-  	})
-  }
+	$.fn.hydrate = function (options,callback) {
+		var filterData = function ($e,filter) {
+			var data = {};
+			$.each($e.data(),function (k,v) { 
+				if(!filter || $.inArray(k,filter) != -1)
+					data[k] = v;
+			});
+			return data;
+		}
+		$.each(["data","withFormData"],function (t) { options[t] ? t : [] })
+
+		$(this).bind({
+			keyup : function (e) { e.keyCode === 13 && $(this).blur() },
+			change :function ()  { 
+				var $t = $(this), 
+					$form = $t.parents("form"), 
+					o = { data : {} };
+				
+				o.data[$t.attr("name") || "value"] = 
+					$t.is(':checkbox,:radio') ? ( $t.attr("value") || $t[0].checked ) : $t.val();
+
+				$form[0] && $.extend(o.data,filterData($form,options.withFormData));
+
+				if(typeof options == "object") {
+
+					$.each(options, function (k,v) {
+						if( k.match( /post|get/ ) )
+							o.method = k, o.url = v;
+						else if( k == "data" )
+							$.extend(o.data, $.isArray(v) ? filterData($t,v) : $t.data());
+						else if( k.match( /success|error/ ) && typeof v == "function" )
+							o[k]=$.proxy(v,$t[0]);
+						else 
+							o[k] = v;
+					});
+				} 
+				else {
+					o.url = options;
+					o.success = $.proxy(callback,$t[0]);
+					$.extend(o.data,$t.data());
+				}
+
+				$.ajax(o);
+			}
+		})
+	}
 })(jQuery)
