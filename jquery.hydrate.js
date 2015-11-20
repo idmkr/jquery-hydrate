ed34cde4c0081691d373305348d689a4b3609484
zzgael <zzgael@gmail.com>
2015-11-20 13:27:38 +0100

    Update jquery.hydrate.js

--- jquery.hydrate.js
+++ jquery.hydrate.js
@@ -3,44 +3,59 @@
 		var filterData = function ($e,filter) {
 			var data = {};
 			$.each($e.data(),function (k,v) { 
-				if(!filter || $.inArray(k,filter) != -1)
+				if( !filter || 
+					( $.isArray(filter) && $.inArray(k,filter) != -1) || 
+					( $.isFunction(filter) && $.proxy(filter,$e,k)() ) || 
+					( $.type(filter) == "regexp" && filter.test(k) ) || 
+					( $.type(filter) == "string" && filter == k ) || 
+				)
 					data[k] = v;
 			});
 			return data;
 		}
-		$.each(["data","withFormData"],function (t) { options[t] ? t : [] })
+
+		var opts = $.extend({
+			dataFilter : false,
+			parent : "form",
+			parentDataFilter : false
+		},options);
 
 		$(this).bind({
 			keyup : function (e) { e.keyCode === 13 && $(this).blur() },
 			change :function ()  { 
 				var $t = $(this), 
-					$form = $t.parents("form"), 
+					$parent = $t.parents(opts.parent), 
 					o = { data : {} };
 				
 				o.data[$t.attr("name") || "value"] = 
-					$t.is(':checkbox,:radio') ? ( $t.attr("value") || $t[0].checked ) : $t.val();
+					$t.is(':checkbox,:radio') ? ( $t.val() || $t[0].checked ) : $t.val();
 
-				$form[0] && $.extend(o.data,filterData($form,options.withFormData));
+				if(typeof opts == "object") {
+					$.each(opts, function (k,v) {
 
-				if(typeof options == "object") {
-
-					$.each(options, function (k,v) {
 						if( k.match( /post|get/ ) )
 							o.method = k, o.url = v;
-						else if( k == "data" )
-							$.extend(o.data, $.isArray(v) ? filterData($t,v) : $t.data());
+
+						else if( k == "dataFilter" )
+							$.extend( o.data, v ? filterData($t,v) : $t.data() );
+
 						else if( k.match( /success|error/ ) && typeof v == "function" )
 							o[k]=$.proxy(v,$t[0]);
-						else 
+
+						else if( k == "parentDataFilter" && opts.parent)
+							$.extend( o.data, v ? filterData($parent,v) : $parent.data() );
+
+						else if( !k.match( /parent/ ) )
 							o[k] = v;
 					});
 				} 
 				else {
-					o.url = options;
+					o.url = opts;
 					o.success = $.proxy(callback,$t[0]);
 					$.extend(o.data,$t.data());
 				}
 
+				console.log(o);return;
 				$.ajax(o);
 			}
 		})
